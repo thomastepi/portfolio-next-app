13bd72afa48233f66f18c8d13d4adad1aa95eac8
thomastepi <tepexminem2013@gmail.com>
2025-06-17 12:38:40 -0400

    remove react hooks from conditionals in i18n client setup

--- src/app/i18n/client.js
+++ src/app/i18n/client.js
@@ -9,20 +9,24 @@ const runsOnServerSide = typeof window === "undefined";
 
 export function useT(ns, options) {
   const lng = useParams()?.lng;
-  if (typeof lng !== "string")
+  if (typeof lng !== "string") {
     throw new Error("useT is only available inside /app/[lng]");
+  }
+
+  const [activeLng, setActiveLng] = useState(i18next.resolvedLanguage);
+  useEffect(() => {
+    if (activeLng === i18next.resolvedLanguage) return;
+    setActiveLng(i18next.resolvedLanguage);
+  }, [activeLng, i18next.resolvedLanguage]);
+
+  useEffect(() => {
+    if (!lng || i18next.resolvedLanguage === lng) return;
+    i18next.changeLanguage(lng);
+  }, [lng, i18next]);
+
   if (runsOnServerSide && i18next.resolvedLanguage !== lng) {
     i18next.changeLanguage(lng);
-  } else {
-    const [activeLng, setActiveLng] = useState(i18next.resolvedLanguage);
-    useEffect(() => {
-      if (activeLng === i18next.resolvedLanguage) return;
-      setActiveLng(i18next.resolvedLanguage);
-    }, [activeLng, i18next.resolvedLanguage]);
-    useEffect(() => {
-      if (!lng || i18next.resolvedLanguage === lng) return;
-      i18next.changeLanguage(lng);
-    }, [lng, i18next]);
   }
+
   return useTranslation(ns, options);
 }
