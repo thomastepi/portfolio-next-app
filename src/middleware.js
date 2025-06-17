d647e46e1635beecb6f0d2c6f9375678f0140cfa
thomastepi <tepexminem2013@gmail.com>
2025-06-17 16:07:38 -0400

    update middleware matcher to include sitemap and robots

--- src/middleware.js
+++ src/middleware.js
@@ -12,7 +12,7 @@ acceptLanguage.languages(languages);
 export const config = {
   // Avoid matching for static files, API routes, etc.
   matcher: [
-    "/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest).*)",
+    "/((?!api|_next/static|_next/image|assets|favicon.ico|robots.txt|sitemap.xml|sw.js|site.webmanifest).*)",
   ],
 };
 
