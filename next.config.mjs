7fbddfb27579482f16a384e844423597471258f1
thomastepi <tepexminem2013@gmail.com>
2025-09-11 23:10:42 -0400

    containerize app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        pathname: "/thormars/**",
+      },
+    ],
+  },
+
+  experimental: {
+    optimizePackageImports: [
+      "@chakra-ui/react",
+      "@emotion/react",
+      "framer-motion",
+    ],
+  },
+  eslint: { ignoreDuringBuilds: true },
+};
 
 export default nextConfig;
